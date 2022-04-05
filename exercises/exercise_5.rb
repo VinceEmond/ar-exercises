9191871d1774ea1c05ee99b1fea81a90934edf67
Vince Emond <vince.emond@icloud.com>
2022-04-05 13:59:55 -0700

    Completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,17 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts "\nExercise 5"
+puts "----------\n\n"
 
-# Your code goes here ...
+puts "\nThe total sum of annual revenue of all stores is: $#{Store.sum(:annual_revenue)}"
+
+puts "\n The average revenue for all stores is #{Store.average(:annual_revenue).to_i}"
+
+# @top_earners = Store.where(annual_revenue: 1000000..Float::INFINITY)
+
+# @top_earners.each do |store|
+#   puts "#{store.name}: #{store.annual_revenue}"
+# end
+
+puts Store.where(annual_revenue: 1000000..Float::INFINITY).count
